4c8627bd3765ea53e60a7505863bb4efd30aa5b8
Hays Clark <hays@omita.com>
2015-04-30 07:05:26 -0700

    stuff

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,19 +1,28 @@
 package;
 
-
 import openfl.display.Sprite;
-
+import openfl.display.Tilesheet;
+import com.marshgames.openfltexturepacker.TexturePackerImport;
 
 class Main extends Sprite {
 	
-	
 	public function new () {
-		
 		super ();
-		
-		
+		draw();
+	}
+
+	function draw() {
+
+		var frames:Array<TexturePackerFrame> = TexturePackerImport.parseJson(
+			openfl.Assets.getText("assets/images.json")
+		);
+
+		var tilesheet:Tilesheet = new Tilesheet(
+		    openfl.Assets.getBitmapData("assets/images.png")
+		);
+
+		var idMap:Map<String, Int> = TexturePackerImport.addToTilesheet(tilesheet, frames);
+		tilesheet.drawTiles(this.graphics, [10, 10, idMap["bookshelf_64px.png"]]);
 		
 	}
-	
-	
 }
